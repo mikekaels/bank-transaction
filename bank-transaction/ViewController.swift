9b2a7a06fa95a0b227a6939ba6e5a838fa22540f
mikekaels <sntmcl1@gmail.com+mikekaels@users.noreply.github.com>
2022-03-10 20:59:40 +0700

    Initial commit

---
+++ bank-transaction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bank-transaction
+//
+//  Created by Santo Michael Sihombing on 10/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
